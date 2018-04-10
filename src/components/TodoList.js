c66cc73dcddadeac5460a71436301a0627c4a1bc
Radek Izdebski <radek1izdebski@gmail.com>
2018-04-10 10:18:40 +0200

    Changed code in TodoList.js

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,7 +3,7 @@ import style from './TodoList.css';
 import Todo from './Todo';
 
 const TodoList = props => {
-    const todoListWorks = props.data.map(work => <Todo id={work.id} name={work.text} remove={props.remove}/>)
+    const todoListTasks = props.data.map(work => <Todo id={task.id} name={task.text} remove={props.remove}/>)
     return (
         <ul className={style.TodoList}>{todoListItems}</ul>
     )
